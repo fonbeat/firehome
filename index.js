8b3e004312aa19c5be8d0038ce5fd68c06a9128e
Derrick Mintah <dmintah@apotica.local>
2021-01-04 09:23:01 +0000

    firehome

---
+++ index.js
@@ -0,0 +1,12 @@
+
+function myFunction(e) {
+    console.log(e);
+    var x = document.getElementById("dropdown1");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+  
